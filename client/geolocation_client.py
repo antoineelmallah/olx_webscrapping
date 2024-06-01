2e749ce3fae4eeb067450d25b47712922cf2343e
Antoine <antoine.elmallah@gmail.com>
2024-06-01 20:30:50 -0300

    Geolocation fallback

--- client/geolocation_client.py
+++ client/geolocation_client.py
@@ -3,28 +3,36 @@ from geopy.geocoders import Nominatim
 import requests
 import os
 
+def get_with_brazilcep(zipcode):
+    address = brazilcep.get_address_from_cep(zipcode)
+    geolocator = Nominatim(user_agent='test_app')
+    text = f'{ address["street"] }, { address["city"] }, { address["district"] }'
+    location = geolocator.geocode(text).raw
+    return float(location['lat']), float(location['lon'])
+
+def get_with_zipcodestack(zipcode):
+    url = 'https://api.zipcodestack.com/v1/search'
+    params = {
+        'codes': zipcode,
+        'country': 'br',
+    }
+    headers = { 'apikey': os.environ['ZIPCODESTACK_API_KEY'] }
+    try:
+        response = requests.request('GET', url, headers=headers, params=params)
+        if not response:
+            return None
+        latlong = response.json()['results'][zipcode][0]
+        return (float(latlong['latitude']), float(latlong['longitude']))
+    except Exception as e:
+        return None
+
+
 def get_geocode(zipcode):
     try:
-        address = brazilcep.get_address_from_cep(zipcode)
-        geolocator = Nominatim(user_agent='test_app')
-        text = f'{ address["street"] }, { address["city"] }, { address["district"] }'
-        location = geolocator.geocode(text).raw
-        return float(location['lat']), float(location['lon'])
+        return get_with_brazilcep(zipcode)
     except Exception as e:
-        url = 'https://api.zipcodestack.com/v1/search'
-        params = {
-            'codes': zipcode,
-            'country': 'br',
-        }
-        headers = { 'apikey': os.environ['ZIPCODESTACK_API_KEY'] }
-        try:
-            response = requests.request('GET', url, headers=headers, params=params)
-            if not response:
-                return None
-            latlong = response.json()['results'][zipcode][0]
-            return (float(latlong['latitude']), float(latlong['longitude']))
-        except Exception as e:
-            return None
+        result = get_with_zipcodestack(zipcode)
+        return result if result else get_with_zipcodestack(zipcode[:5] + '000')
 
 if __name__ == '__main__':
     print(get_geocode('25655151'))
