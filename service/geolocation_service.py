0ff7ca3d005a3768a861432116ef77c40e25a71d
Antoine <antoine.elmallah@gmail.com>
2024-08-31 09:19:02 -0300

    Validate geocode

--- service/geolocation_service.py
+++ service/geolocation_service.py
@@ -5,6 +5,15 @@ import traceback
 
 log = logging.getLogger(__name__)
 
+def valid_rj_state_geolocation(geocode):
+    if not geocode:
+        return False
+    lat = geocode[0]
+    lon = geocode[1]
+    valid_lat = lat > -23.39 and lat < -20.78
+    valid_lon = lon > -44.88 and lon < -40.97
+    return valid_lat and valid_lon
+
 @retry(exceptions=(TypeError), tries=3, delay=2, logger=log)
 def resolve_geolocation(adv):
     if not adv.zipcode:
@@ -14,7 +23,7 @@ def resolve_geolocation(adv):
         log.info(f'*** FINDING LOCATION')
         try:
             geocode = get_geocode(adv.zipcode)
-            if geocode:
+            if valid_rj_state_geolocation(geocode):
                 adv.lat = geocode[0]
                 adv.lon = geocode[1]
         except TypeError as e:
